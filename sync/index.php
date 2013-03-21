4cd5bd1928a1ef007f3af94faaf58875d142a112
macarena <marcoaasilva@gmail.com>
2013-03-21 01:45:10 -0300

    api para sincronismo de cordenadas e pins do post

---
+++ sync/index.php
@@ -0,0 +1,44 @@
+<head>
+<title>Sync</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<form enctype="multipart/form-data">
+<?php
+ini_set('default_charset', 'UTF-8');
+
+include_once '../colab/db.php';
+mysql_connect($db_host,$db_username,$db_password);
+mysql_select_db($db_name) or die(mysql_error());
+
+//Listando todos os projetos por local
+$result = mysql_query("SELECT id, post_title FROM `wp_posts` WHERE post_type LIKE 'post'");
+
+while($row = mysql_fetch_assoc($result)) {
+	$projetos[$row[id]] = $row;
+}
+
+foreach($projetos as $projeto) {
+	$id = $projeto[id];
+	//$json = file_get_contents("http://199.127.227.146/bxc2013/colab!projetoMapa.action?codProjetoMysql=$id");
+	$values = json_decode($json, true);
+	$values = $values[values];
+	$lugar = $values[nomLocal];
+	
+	$result2 = mysql_query("SELECT meta_id FROM `wp_postmeta` WHERE post_id LIKE '$id' AND meta_key LIKE '_mpv_location'");
+	//print_r($result2);
+	
+	$row = mysql_num_rows($result2);
+	
+	if ($row > 0) echo "<input type=\"hidden\" name=\"projetos[$id][pin]\" value=\"true\">";
+	
+	echo "<input type=\"hidden\" name=\"projetos[$id][id]\" value=\"$id\">";
+	echo "<input type=\"hidden\" name=\"projetos[$id][nome]\" value=\"$projeto[post_title]\">";
+	echo "$id - $projeto[post_title] - $lugar<br />\n";
+	echo "Lat: <input type=\"text\" name=\"projetos[$id][lat]\"> - Long: <input type=\"text\" name=\"projetos[$id][long]\"><br />\n";
+}
+?>
+ <button type="submit" formaction="sync.php" formmethod="post">Syncar!</button> 
+</form>
+
+</body>
---
+++ sync/sync.php
@@ -0,0 +1,24 @@
+<?php
+include_once '../colab/db.php';
+mysql_connect($db_host,$db_username,$db_password);
+mysql_select_db($db_name) or die(mysql_error());
+
+$projetos = $_POST[projetos];
+
+foreach ($projetos as $projeto) {
+	$id = $projeto[id];
+	$lat = $projeto[lat];
+	$long = $projeto[long];
+	if($projeto[pin] == true) {
+		mysql_query("UPDATE wp_postmeta
+					SET meta_value='a:2:{s:3:\"lat\";d:$lat;s:3:\"lon\";d:$long;}'
+					WHERE post_id=$id AND meta_key='mpv_location'") or die(mysql_error());
+		echo "<p><small>Pin do projeto $projeto[nome] atualizado com sucesso.</p></small>";
+	} else {
+		mysql_query("INSERT INTO wp_postmeta
+					VALUES (NULL, $id, '_mpv_inmap', '1439')") or die(mysql_error());
+		mysql_query("INSERT INTO wp_postmeta
+				VALUES (NULL, $id, '_mpv_location', 'a:2:{s:3:\"lat\";d:$lat;s:3:\"lon\";d:$long;}')") or die(mysql_error());
+		echo "<p><small>Pin do projeto $projeto[nome] inserido com sucesso.</p></small>";
+	}
+}
